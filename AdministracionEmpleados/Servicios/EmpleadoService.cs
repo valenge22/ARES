edda79b7f47b72cb0127ce67f5bd631f7e6ba2d0
valenge22 <valenge22@users.noreply.github.com>
2026-08-02 05:18:26 -0300

    Eliminar equipos de demostración

--- AdministracionEmpleados/Servicios/EmpleadoService.cs
+++ AdministracionEmpleados/Servicios/EmpleadoService.cs
@@ -8,50 +8,7 @@ namespace AdministracionEmpleados.Servicios
 
         public EmpleadoService()
         {
-            empleados = new List<Empleado>
-            {
-                new Empleado
-                {
-                    Nombre = "Valentín",
-                    Computadora = new Computadora
-                    {
-                        Nombre = "PC-01",
-                        DireccionIP = "192.168.1.10",
-                        EstaEncendida = true,
-                        EstaLogueada = true,
-                        EstaBloqueada = false,
-                        SistemaOperativo = "Windows 11"
-                    }
-                },
-
-                new Empleado
-                {
-                    Nombre = "Melany",
-                    Computadora = new Computadora
-                    {
-                        Nombre = "PC-02",
-                        DireccionIP = "192.168.1.11",
-                        EstaEncendida = true,
-                        EstaLogueada = true,
-                        EstaBloqueada = true,
-                        SistemaOperativo = "Windows 11"
-                    }
-                },
-
-                new Empleado
-                {
-                    Nombre = "Agostina",
-                    Computadora = new Computadora
-                    {
-                        Nombre = "PC-03",
-                        DireccionIP = "192.168.1.12",
-                        EstaEncendida = false,
-                        EstaLogueada = false,
-                        EstaBloqueada = true,
-                        SistemaOperativo = "Windows 10"
-                    }
-                }
-            };
+            empleados = new List<Empleado>();
         }
 
         public List<Empleado> ObtenerEmpleados()
